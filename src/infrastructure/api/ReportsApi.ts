149eef88105c82c53523bad2c955cd79a28e80c7
Malu Matos <maria.matos@portalmedico.org.br>
2024-03-23 12:22:16 -0300

    remove unused imports

--- src/infrastructure/api/ReportsApi.ts
+++ src/infrastructure/api/ReportsApi.ts
@@ -1,23 +1,13 @@
 import {
   Body,
   Controller,
-  Get,
   HttpStatus,
-  Inject,
-  Param,
   Post,
-  Put,
-  Query,
-  Req,
   Res,
 } from '@nestjs/common';
-import { TimeRecordCreationDTO } from 'src/core/application/dto/TimeRecordCreationDTO';
-import { TimeRecordResponseDTO } from 'src/core/application/dto/TimeRecordResponseDTO';
-import { RecordsController } from '../controller/RecordsController';
-import { IConnection } from '../adapters/external/IConnection';
 import { MessageProducer } from '../adapters/external/MessageProducer';
-import {ReportTimeSheetController} from "../controller/ReportTimeSheetController";
-import {ReportRequestDTO} from "../../core/application/dto/ReportRequestDTO";
+import { ReportTimeSheetController } from "../controller/ReportTimeSheetController";
+import { ReportRequestDTO } from "../../core/application/dto/ReportRequestDTO";
 
 @Controller('time-sheet/reports/')
 export default class RecordsApi {
